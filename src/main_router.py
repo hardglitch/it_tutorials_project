818a64f1966612a1301d2d7dad71d736aa5c483b
lg-lg-lg <ls45t6ypi21shh@protonmail.com>
2023-04-08 18:02:14 +0500

    add auth

--- src/main_router.py
+++ src/main_router.py
@@ -5,11 +5,3 @@ from src.user.router import user_router
 class MainRouter:
     def __init__(self, app: FastAPI):
         app.include_router(user_router)
-
-        # @app.get("/")
-        # async def root():
-        #     return {"message": "Hello World"}
-        #
-        # @app.get("/hello/{name}")
-        # async def say_hello(name: str):
-        #     return {"message": f"Hello {name}"}
