1f11ff72a3c575021c3b84c65b498ee77d71090f
lg-lg-lg <ls45t6ypi21shh@protonmail.com>
2023-05-12 19:39:39 +0500

    Main page and auth popup are designed

--- app/main.py
+++ app/main.py
@@ -2,17 +2,18 @@
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-from starlette.staticfiles import StaticFiles
+# from starlette.staticfiles import StaticFiles
 from .router import MainRouter
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
+from .startup import lifespan
 
 
-app = FastAPI(title="IT Tutorials")
+app = FastAPI(title="IT Tutorials", lifespan=lifespan)
 templates_dir = Path(__name__.split(".")[0]).joinpath("templates")
 templates = Jinja2Templates(directory=templates_dir)
-app.mount("/static", StaticFiles(directory=Path(__name__.split(".")[0]).joinpath("static")), name="static")
+# app.mount("/static", StaticFiles(directory=Path(__name__.split(".")[0]).joinpath("static")), name="static")
 
 app.add_middleware(HTTPSRedirectMiddleware)
 app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost"])  # "example.com", "*.example.com"
---
+++ app/startup.py
@@ -0,0 +1,19 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio
+from app.config import REDIS_HOST, REDIS_PASS, REDIS_PORT
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> None:
+    redis = asyncio.from_url(
+        url=f"redis://{REDIS_PASS}@{REDIS_HOST}:{REDIS_PORT}/0",
+        encoding="utf-8",
+        decode_responses=True
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    yield
+    await FastAPICache.clear()
+
