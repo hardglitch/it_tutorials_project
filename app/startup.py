31e756559f23b30a59a0999322d776358a7ac3b7
lg-lg-lg <ls45t6ypi21shh@protonmail.com>
2023-05-28 14:01:12 +0500

    startup.py prepared to deploy

--- app/startup.py
+++ app/startup.py
@@ -8,8 +8,9 @@ from app.config import REDIS_HOST, REDIS_PASS, REDIS_PORT
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> None:
+    redis_pass_str = f":{REDIS_PASS}@" if REDIS_PASS else ""
     redis = aioredis.from_url(
-        url=f"redis://:{REDIS_PASS}@{REDIS_HOST}:{REDIS_PORT}",
+        url=f"redis://" + redis_pass_str + f"{REDIS_HOST}:{REDIS_PORT}",
         encoding="utf-8",
         decode_responses=False,
     )
